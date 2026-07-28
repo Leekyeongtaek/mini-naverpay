92a28f68ad3e80f2ba38589746341260401c9a5b
mrlee <lkt900520@gmail.com>
2026-07-28 10:57:32 +0900

    feat: 프로젝트 기본 환경 세팅

---
+++ src/main/java/com/minipay/MiniNaverPayApplication.java
@@ -0,0 +1,13 @@
+package com.minipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniNaverPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniNaverPayApplication.class, args);
+    }
+
+}
