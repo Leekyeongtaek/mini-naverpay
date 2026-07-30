91d26686a992e946187c46198420c351663e4d67
mrlee <lkt900520@gmail.com>
2026-07-30 11:51:45 +0900

    test: 포인트 충전 낙관적 락 테스트 코드

--- mini-pay-point/src/test/java/com/minipay/point/service/PointServiceConcurrencyTest.java
+++ mini-pay-point/src/test/java/com/minipay/point/service/PointServiceConcurrencyTest.java
@@ -2,9 +2,9 @@ package com.minipay.point.service;
 
 import com.minipay.common.domain.Point;
 import com.minipay.point.dto.PointChargeRequest;
+import com.minipay.point.facade.PointOptimisticLockFacade;
 import com.minipay.point.repository.PointHistoryRepository;
 import com.minipay.point.repository.PointRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +15,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class PointServiceConcurrencyTest {
@@ -28,6 +28,9 @@ public class PointServiceConcurrencyTest {
 
     @Autowired
     private PointHistoryRepository pointHistoryRepository;
+
+    @Autowired
+    private PointOptimisticLockFacade pointOptimisticLockFacade;
 
     @AfterEach
     void tearDown() {
@@ -93,6 +96,45 @@ public class PointServiceConcurrencyTest {
             executorService.execute(() -> {
                 try {
                     pointService.chargePointWithPessimisticLock(new PointChargeRequest(userId, chargeAmount));
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Point point = pointRepository.findByUserId(userId).orElseThrow();
+        Long expectedBalance = chargeAmount * threadCount;
+
+        System.out.println("==========================================");
+        System.out.println("기대 포인트 : " + expectedBalance); // 10000
+        System.out.println("실제 충전 포인트 : " + point.getAmount()); // 10000
+        System.out.println("==========================================");
+
+        assertThat(point.getAmount()).isEqualTo(expectedBalance);
+    }
+
+    @Test
+    @DisplayName("낙관적 락 적용: 버전 충돌 시 Facade 재시도를 통해 100% 데이터 정합성 보장")
+    void chargePoint_concurrency_with_optimistic_lock() throws InterruptedException {
+        // given
+        Long userId = 1L;
+        Long chargeAmount = 100L;
+        int threadCount = 100;
+
+        pointRepository.save(new Point(userId, 0L));
+
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // when
+        for (int i = 0; i < threadCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    pointOptimisticLockFacade.chargePoint(new PointChargeRequest(userId, chargeAmount));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 } finally {
                     latch.countDown();
                 }
